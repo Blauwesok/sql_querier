a45ad81fa596c628f5828b36649236b5d6713953
Rijk van Putten <rijk@rijkvp.nl>
2022-05-08 09:02:16 +0000

    Fix startup script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -13,7 +13,7 @@ Door: Rijk van Putten
 echo "Starting MySQL server.."               
 ./scripts/start_mysql.sh
 echo "Importing/resetting MySQL databases.."
-./scripts/reset_dbs.sh
+./scripts/restore_dbs.sh
 
 cd ./mysql-querier
 echo "Downloading Node.js packages.."
